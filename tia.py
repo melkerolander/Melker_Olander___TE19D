823890fa53c39d4c4a0d04030a56d32b3332e33d
melkerolander <70263740+melkerolander@users.noreply.github.com>
2020-11-26 09:38:48 +0100

    wewe

---
+++ tia.py
@@ -0,0 +1,94 @@
+s = 1
+for i in range (1,10,2):
+    s = s* i
+
+print(s)
+
+'''
+wewewe
+'''
+
+s = 10000
+förändringsfaktor = 1.03
+
+for i in range (15):
+    s = förändringsfaktor* s
+
+print(s)
+
+'''
+wewewe1
+'''
+x = int(input("vilken multiplikationstabell"))
+end = int(input("vart ska taellen avslutas?"))
+if x > 15:
+    print("multiplikationstabellen fungerar inte om x är mer än 15")
+else:
+    for i in range (1,end+1):
+        tabell = x*i
+        print(tabell, end=" ")
+
+'''
+wewewe
+'''
+tal = 11
+
+while tal >1:
+    tal -=1
+
+    print(tal)
+
+
+'''
+m04u05
+'''
+
+råttor = 100
+månad = 0
+while råttor <1000000:
+    råttor = råttor*2
+    månad += 1
+
+print(månad)
+
+'''
+m0
+'''
+
+summa = 10000
+år = 0
+while år <15:
+    summa = summa*1.03
+    år += 1
+
+print(summa)
+
+'''
+wewe
+'''
+
+månad = 0
+råttor = 100
+for i in range (1000):
+    råttor = råttor*2
+    månad += 1
+    if råttor >1000000:
+        break
+
+print(månad)
+
+'''
+erer
+'''
+starttal = int(input(""))
+sluttal = int(input(""))
+skiptal = int(input(""))
+avbryt = int(input(""))
+
+while starttal < sluttal:
+    sluttal = 1
+    if 
+    
+
+
+
